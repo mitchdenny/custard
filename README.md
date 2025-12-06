bc4b8d3580a6525585a7222a0b660ae86b177553
Mitch Denny <mitch@mitchdeny.com>
2025-12-06 19:59:22 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Custard
+
+Custard is a .NET library for creating beautiful TUI apps with an API surface which is inspired by React-like client rendering frameworks.
+
+## Installation
+
+```dotnetcli
+dotnet add Custard
+```
+
+## Usage
+
+TODO:
---
+++ samples/Cancellation/Program.cs
@@ -0,0 +1,16 @@
+﻿using Custard;
+
+// Set up cancellation with Ctrl+C
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true; // Prevent immediate termination
+    cts.Cancel();
+};
+
+// Create and run the app
+using var app = new CustardApp(App);
+await app.RunAsync(cts.Token);
+
+// The root component
+static Task<CustardWidget> App(CancellationToken cancellationToken) => CustardWidgets.TextBlockAsync("Hello, Custard!", cancellationToken);

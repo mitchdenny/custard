7cc5346924e049aedd6e6e10238be69c407ffa50
Mitch Denny <mitch@mitchdeny.com>
2025-12-17 14:35:53 +1100

    Remove unnecessary API.

---
+++ src/Hex1b/Hex1bWidgets.cs
@@ -0,0 +1,23 @@
+using Hex1b.Input;
+using Hex1b.Widgets;
+
+namespace Hex1b;
+
+public static class Hex1bWidgets
+{
+    public static Task<Hex1bWidget> TextBlockAsync(string text, CancellationToken cancellationToken = default) => Task.FromResult<Hex1bWidget>(new TextBlockWidget(text));
+
+    public static Task<Hex1bWidget> TextBoxAsync(TextBoxState state, CancellationToken cancellationToken = default) => Task.FromResult<Hex1bWidget>(new TextBoxWidget(state));
+
+    public static Task<Hex1bWidget> VStackAsync(IReadOnlyList<Hex1bWidget> children, CancellationToken cancellationToken = default) => Task.FromResult<Hex1bWidget>(new VStackWidget(children));
+
+    public static Task<Hex1bWidget> VStackAsync(CancellationToken cancellationToken = default, params Hex1bWidget[] children) => Task.FromResult<Hex1bWidget>(new VStackWidget(children));
+
+    public static Task<Hex1bWidget> HStackAsync(IReadOnlyList<Hex1bWidget> children, CancellationToken cancellationToken = default) => Task.FromResult<Hex1bWidget>(new HStackWidget(children));
+
+    public static Task<Hex1bWidget> HStackAsync(CancellationToken cancellationToken = default, params Hex1bWidget[] children) => Task.FromResult<Hex1bWidget>(new HStackWidget(children));
+
+    public static Task<Hex1bWidget> ButtonAsync(string label, Action<ActionContext> onClick, CancellationToken cancellationToken = default) => Task.FromResult<Hex1bWidget>(new ButtonWidget(label) { OnClick = ctx => { onClick(ctx); return Task.CompletedTask; } });
+    
+    public static Task<Hex1bWidget> ButtonAsync(string label, Func<ActionContext, Task> onClick, CancellationToken cancellationToken = default) => Task.FromResult<Hex1bWidget>(new ButtonWidget(label) { OnClick = onClick });
+}
